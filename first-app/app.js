61d885b1b9ca0d769e6ab6723757907623bdf4cf
Uros Dimitrijevic <31245139+UrosDimitrijevic31@users.noreply.github.com>
2020-12-22 16:01:16 +0100

    Add files via upload

---
+++ first-app/app.js
@@ -0,0 +1,25 @@
+const path = require('path'); //za rad sa putanjama direktorijuma i fajlova... putanje, nazivi, ekstenzije
+
+const os = require('os'); //informacije o operativnom sistemu, ako ne koristimo node ne moze da ih dobijemo jer js radi u browseru
+
+const fs = require('fs'); //rad sa fajlovima
+
+//klasa EventerEmmiter
+const EventEmmiter = require('events'); 
+
+// on uvek mora da ide pre emit, emmit poziva ucitane on listenere redom, da je pre ne bi imao nijedan da pozove, ne moze da gleda kod unapred
+//addListener je isto sto i on, al se on cesce koristi
+// emmiter.on('Nesto se desilo', (arrg) => { 
+//     console.log('detektovana promena', arrg);
+// })
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('Nesto se desilo', (arg) => {
+    console.log('Detektovana promena', arg);
+})
+
+logger.log(('poruka poslata iz app.js'));
+ 
+
